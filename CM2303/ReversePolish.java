a9d2de8a7550fd19021404e884cbc2061823aafa
Paul Rosin <101402865+rosinpl@users.noreply.github.com>
2025-10-03 16:51:48 +0100

    Update CM2303/ReversePolish.java

---
+++ CM2303/ReversePolish.java
@@ -0,0 +1,54 @@
+/* Reverse Polish calculator
+ * implemented using stacks
+ *
+ * Paul Rosin
+ * Cardiff University
+ */
+import java.io.*;
+
+public class ReversePolish {
+
+    public static void main(String [] args) throws Exception 
+    {
+        ArrayStack stack = new ArrayStack(100);
+        Double a,b;
+        double va,vb,vv=0;
+        char ch;
+
+        StreamTokenizer parser = new StreamTokenizer(System.in);
+        parser.ordinaryChar('/');
+
+        System.out.println("type in postfix expression:");
+
+        while ( parser.nextToken() != StreamTokenizer.TT_EOF ) {
+            if  ( parser.ttype == StreamTokenizer.TT_NUMBER ) {
+                System.out.println( "read a number: " + parser.nval );
+                stack.push(new Double(parser.nval));
+            }
+            else {
+                System.out.println( "read: " + (char) parser.ttype);
+                ch = (char) parser.ttype;
+                if ((ch == '+') || (ch == '-') || (ch == '*')|| (ch == '/')) {
+                    a = (Double) stack.pop();
+                    b = (Double) stack.pop();
+                    System.out.println("evaluating ("+b+" "+ch+" "+a+")");
+                    va = a.doubleValue();
+                    vb = b.doubleValue();
+                    switch(ch) {
+                        case '+': vv = vb + va; break;
+                        case '-': vv = vb - va; break;
+                        case '*': vv = vb * va; break;
+                        case '/': vv = vb / va; break;
+                    }
+                    stack.push(new Double(vv));
+                    System.out.println("= "+vv);
+                }
+                else {
+                    throw new Exception(ch + " is an invalid character");
+                }
+            }
+        }
+        a = (Double) stack.pop();
+        System.out.println("result = "+a);
+    }
+}
